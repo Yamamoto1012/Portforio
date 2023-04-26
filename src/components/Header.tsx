92a02319f3650ee7707e170d4177ba25386b46ef
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2023-04-27 03:41:19 +0900

    reset.css標準搭載しているのに、追加してしまったけど、とりあえずコミット

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,9 @@ export default function App() {
     return (
         <div>
             <h1 className="font-kaisei text-3xl font-bold underline">React App</h1>
+            <nav>
+                <ul></ul>
+            </nav>
         </div>
     );
 };
