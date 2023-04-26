b0df3a1ebfd6de9cd0205c21fe534b49e065f239
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2023-04-27 02:53:58 +0900

    tailwindにgooleフォントを導入した

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">React App</h1>
+      <h1 className="font-kaisei text-3xl font-bold underline">React App</h1>
     </div>
   )
 };
